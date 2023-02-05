aa286fda6e1dbfec3a92949307af67bf4be4b3d4
Flyerbook <111646408+Flyerbook@users.noreply.github.com>
2023-02-05 19:30:31 +0000

    fixes README

--- src/products/ImageService.ts
+++ src/products/ImageService.ts
@@ -14,4 +14,24 @@ export interface SignedUrl {
 
 const S3_REGION = process.env.S3_REGION;
 const S3_BUCKET = process.env.S3_BUCKET;
-const S3_ACCESS_KEY = process.
+const S3_ACCESS_KEY = process.env.S3_ACCESS_KEY;
+const S3_SECRET_KEY = process.env.S3_SECRET_KEY;
+const EXPIRES = 300;    // 5 minutes
+let s3Client: aws.S3 | null = null;
+
+// These values must be initialize to use this module
+try {
+    if (S3_REGION != null && S3_BUCKET != null && S3_ACCESS_KEY != null && S3_ACCESS_KEY != null && S3_SECRET_KEY != null) {
+        s3Client = new aws.S3({
+            region : S3_REGION,
+            credentials: {
+                accessKeyId: S3_ACCESS_KEY,
+                secretAccessKey: S3_SECRET_KEY
+            }
+        });
+    } else {
+        appLogger.warn("ImageService not initialized. Missing environment configuration.");
+    }
+} catch (err) {
+    // Invalid configuration
+    appLogger.err
