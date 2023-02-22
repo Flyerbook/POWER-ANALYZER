17748b4667cf1fe8f87730e9adb6338781dbc7c2
Flyerbook <111646408+Flyerbook@users.noreply.github.com>
2023-02-22 10:02:02 +0000

    Minor

--- src/products/tagModel.ts
+++ src/products/tagModel.ts
@@ -4,4 +4,33 @@ import { registerAssociations, registerModel } from "../sequelize";
 import { Product, PRODUCT_FK } from "./productModel";
 
 export class Tag extends Model<InferAttributes<Tag>, InferCreationAttributes<Tag>> {
-    declare productId: CreationOpt
+    declare productId: CreationOptional<UUID>;
+    declare name: string;
+    declare value: string;
+
+    /** Retrieve the associated Product. */
+    declare getProduct: BelongsToGetAssociationMixin<Product>;
+
+    // Eager loaded properties.
+    declare product?: NonAttribute<Product>;
+
+    declare static associations: {
+        product: Association<Tag, Product>;
+    }
+}
+
+registerModel(initTagModel);
+registerAssociations(initTagAssociations);
+
+async function initTagModel(sequelize: Sequelize): Promise<void> {
+    Tag.init(
+        {
+            productId: {
+                type: DataTypes.UUID,
+                primaryKey: true,
+                defaultValue: UUIDV4,
+                validate: {
+                    isUUID: 4
+                }
+            },
+      
