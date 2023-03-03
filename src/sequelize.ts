c1622ed65c78ec60d1e19cae0f082c93d90ae766
Flyerbook <111646408+Flyerbook@users.noreply.github.com>
2023-03-03 08:45:56 +0000

    Add anchor

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -12,4 +12,26 @@ const options: Options = config.options;
 type AssociationsInit = () => Promise<void>
 
 /**
- * Receives a Sequelize instance to initi
+ * Receives a Sequelize instance to initialize the Model.
+ * @param seq The Sequelize instance.
+ * @returns A promise to be either resolved after the Model is initialized or rejected with an Error.
+ */
+ type ModelInit = (seq: Sequelize) => Promise<void>;
+ const models: ModelInit[] = [];
+ const associations: AssociationsInit[] = [];
+
+/**
+ * Models defined elsewhere call this method to sync with the database during initialization. 
+ * 
+ * @param init Function that takes a sequelize instance and initializes the Model.
+ */
+export function registerModel(init: ModelInit) {
+    models.push(init);
+}
+
+/**
+ * Model associations defined elsewhere call this method to sync with the database during initialization.
+ * 
+ * @param init Function that initializes the Model's associations.
+ */
+export function register
