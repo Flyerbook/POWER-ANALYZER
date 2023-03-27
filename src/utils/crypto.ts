3fb9c68392c5d42971d61dc8e924f0f9e4649624
Flyerbook <111646408+Flyerbook@users.noreply.github.com>
2023-03-27 19:17:48 +0000

    Update snapshots

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -15,4 +15,21 @@ export async function validateData(data: string | Buffer, encrypted: string): Pr
 
 /**
  * @param data The data to be encrypted.
- * @returns A pr
+ * @returns A promise to be either resolved with the encrypted data or rejected with an Error.
+ */
+export async function hashData(data: string | Buffer): Promise<string> {
+    return await bcrypt.hash(data, SALT_ROUNDS);
+}
+
+/**
+ * Source: https://stackoverflow.com/questions/8855687/secure-random-token-in-node-js
+ * 
+ * @returns A promise to be either resolved with a random token or rejected with an Error.
+ */
+export async function randomToken(tokenSize: number = DEFAULT_TOKEN_SIZE): Promise<string> {
+    return new Promise((resolve, reject) => {
+        crypto.randomBytes(tokenSize, (err: any, buf: Buffer) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(buf.toString("hex")
