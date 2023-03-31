3474c467d69343b84f4898ade39f3637033a9f65
Flyerbook <111646408+Flyerbook@users.noreply.github.com>
2023-03-31 20:17:07 +0000

    Auto-commit (every 10 mins)

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -6,3 +6,38 @@ const logFormat = winston.format.printf(({timestamp, label, level, message}) => 
     if (typeof message == "object") {
         message = JSON.stringify(message, null);
     }
+    return `${timestamp} | ${label} | ${level}: ${message}`
+});
+
+export const appLogger = winston.createLogger({
+    level: "debug",
+    transports: [
+        new winston.transports.Console()
+    ],
+    format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple(),
+        winston.format.timestamp(),
+        winston.format.label({label: "App"}),
+        logFormat,
+    )
+})
+
+export const databaseLogger = winston.createLogger({
+    level: "info",
+    transports: [
+        new winston.transports.Console()
+    ],
+    format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple(),
+        winston.format.timestamp(),
+        winston.format.label({label: "Database"}),
+        logFormat,
+    )
+})
+
+export const routeLogger = expressWinston.logger({
+    winstonInstance: appLogger,
+    msg: `{{req.method}} {{req.path}} {{res.statusCode}} {{res.responseTime}}ms [{{req.ip}}]`,
+});
